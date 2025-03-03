1ce566e405e27d077f2c406d23d72a423b4eb25e
Калашникова Варвара Алексеевна <ip317s08@csc.sibsutis.ru>
2025-03-03 16:27:32 +0700

    create sc_commandDecode function; create GET_BIT macros

--- mySimpleComputer/command.c
+++ mySimpleComputer/command.c
@@ -1,6 +1,7 @@
 #include "stddef.h"
 
 #define SET_MASK(k) 1 << (k - 1)
+#define GET_BIT(value, k) (value >> (k - 1)) & 0x1
 
 int sc_commandEncode(int sign, int command, int operand, int *value) {
   if (sign < 0 || sign > 1)
@@ -14,13 +15,13 @@ int sc_commandEncode(int sign, int command, int operand, int *value) {
   int currentBit = 1;
 
   for (int i = 1; i < 8; i++) {
-    if ((operand >> (i - 1)) & 0x1)
+    if (GET_BIT(operand, i))
       *value |= SET_MASK(currentBit++);
     else
       *value &= ~SET_MASK(currentBit++);
   }
   for (int i = 1; i < 8; i++) {
-    if ((command >> (i - 1)) & 0x1)
+    if (GET_BIT(operand, i))
       *value |= SET_MASK(currentBit++);
     else
       *value &= ~SET_MASK(currentBit++);
@@ -30,5 +31,30 @@ int sc_commandEncode(int sign, int command, int operand, int *value) {
     *value |= SET_MASK(currentBit);
   else
     *value &= ~SET_MASK(currentBit);
+  return 0;
+}
+
+int sc_commandDecode(int value, int *sign, int *command, int *operand) {
+  if (value < 0 || value > (1 << 15) - 1)
+    return -1;
+  if (sign == NULL, command == NULL, operand == NULL)
+    return -1;
+  *sign = *command = *operand = 0;
+  int currentBit = 1;
+  for (int i = 0; i < 8; i++) {
+    if (GET_BIT(value, currentBit++))
+      *operand |= SET_MASK(i);
+    else
+      *operand &= ~SET_MASK(i);
+  }
+
+  for (int i = 0; i < 8; i++) {
+    if (GET_BIT(value, currentBit++))
+      *command |= SET_MASK(i);
+    else
+      *command &= ~SET_MASK(i);
+  }
+
+  *sign = GET_BIT(value, currentBit++);
   return 0;
 }
