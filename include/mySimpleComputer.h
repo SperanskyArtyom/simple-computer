3ca589d2a6211cb7a5e74edb5155dda5ad727872
Калашникова Варвара Алексеевна <ip317s08@csc.sibsutis.ru>
2025-03-03 16:53:00 +0700

    add functions declaration in mySimpleComputer.h

---
+++ include/mySimpleComputer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+int sc_memoryInit(void);
+int sc_memorySet(int address, int value);
+int sc_memoryGet(int address, int *value);
+int sc_memorySave(char *filename);
+int sc_memoryLoad(char *filename);
+
+int sc_regInit(void);
+int sc_regSet(int register, int value);
+int sc_regGet(int register, int *value);
+
+int sc_accumulatorInit(void);
+int sc_accumulatorSet(int value);
+int sc_accumulatorGet(int *value);
+
+int sc_icounterInit(void);
+int sc_icounterSet(int value);
+int sc_icounterGet(int *value);
+
+int sc_commandEncode(int sign, int command, int operand, int *value);
+int sc_commandDecode(int value, int *sign, int *command, int *operand);
+int sc_commandValidate(int command);
