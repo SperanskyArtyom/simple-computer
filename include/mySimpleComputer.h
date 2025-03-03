432c14e8e0f69bb5789b56b3696d0715b68ff5ab
Калашникова Варвара Алексеевна <ip317s08@csc.sibsutis.ru>
2025-03-03 22:23:18 +0700

    fix gcc warning

--- include/mySimpleComputer.h
+++ include/mySimpleComputer.h
@@ -13,8 +13,8 @@ int sc_memorySave(char *filename);
 int sc_memoryLoad(char *filename);
 
 int sc_regInit(void);
-int sc_regSet(int register, int value);
-int sc_regGet(int register, int *value);
+int sc_regSet(int flag, int value);
+int sc_regGet(int flag, int *value);
 
 int sc_accumulatorInit(void);
 int sc_accumulatorSet(int value);
