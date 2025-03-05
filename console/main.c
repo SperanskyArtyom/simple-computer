cebde33d99dee05aecd1f8021fbb21389e9176e1
Сперанский Артём Александрович <ip317s17@csc.sibsutis.ru>
2025-03-05 19:45:05 +0700

    set random values in flags; use regSet with invalid flag and print status of the function

--- console/main.c
+++ console/main.c
@@ -21,14 +21,26 @@ int main() {
   printf("Memory:\n");
   for (int i = 0; i < 128; i++) {
     printCell(i);
-    if ((i + 1) % 10 == 0) {
+    if ((i + 1) % 10 == 0)
       printf("\n");
-    } else
+    else
       printf(" ");
   }
 
   int commandStatus = sc_memorySet(0, -1);
-  printf("\nStatus: %d\n", commandStatus);
+  printf("\n\nStatus of sc_memorySet with invalid value: %d\n", commandStatus);
+
+  sc_regSet(FLAG_OVERFLOW, rand() % 2);
+  sc_regSet(FLAG_DIV_ZERO, rand() % 2);
+  sc_regSet(FLAG_MEM_OOB, rand() % 2);
+  sc_regSet(FLAG_INVALID_CMD, rand() % 2);
+  sc_regSet(FLAG_IGNORE_CLOCK, rand() % 2);
+
+  printf("\nFlags: ");
+  printFlags();
+
+  commandStatus = sc_regSet(10, 1);
+  printf("\n\nStatus of sc_regSet with invalid flag: %d\n", commandStatus);
 
   return 0;
 }
