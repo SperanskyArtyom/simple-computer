9e93740e511c51810fb6bad5ae401f4400bbc40c
Варвара <ip317s08.csc@mail.ru>
2025-03-05 11:04:06 +0300

    initialize the RAM, accumulator, code counter, and flag register; set an arbitrary number of arbitrary operational memory cells to arbitrary values; make output of the operational memory

--- console/main.c
+++ console/main.c
@@ -1,1 +1,28 @@
-int main() { return 0; }
+#include "console.h"
+#include <mySimpleComputer.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main() { 
+    int sc_memoryInit();
+    int sc_accumulatorInit();
+    int sc_icounterInit();
+    int sc_regInit();
+
+    srand(time (NULL));
+    int k = rand()%128+1;
+    for(int i=0; i<k; i++){
+        int value=rand()%(1 << 15);
+        sc_memorySet(i, value);
+    }
+    for(int i=0; i<128; i++){
+        void printCell(i);
+        if ((i+1)%10==0){
+            printf("\n");
+        }
+        else printf(" ");
+    }
+    return 0; }
+
