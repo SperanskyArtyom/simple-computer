2df6365104f0226cf305cdf6924c5dac252a43b4
Калашникова Варвара Алексеевна <ip317s08@csc.sibsutis.ru>
2025-03-13 00:51:34 +0700

    create enum of colors

--- include/myTerm.h
+++ include/myTerm.h
@@ -1,5 +1,17 @@
 #pragma once
 
+enum color
+{
+  BLACK,
+  RED,
+  GREEN,
+  YELLOW,
+  BLUE,
+  MAGENTA,
+  CYAN,
+  WHITE,
+};
+
 int mt_clrscr (void);
 int mt_gotoXY (int, int);
 int mt_getscreensize (int *rows, int *cols);
