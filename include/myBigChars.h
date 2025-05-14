835ed0599c0dbebc5778127e9903d6b778df774d
Сперанский Артём Александрович <ip317s17@csc.sibsutis.ru>
2025-05-15 03:10:23 +0700

    add bc_box function

--- include/myBigChars.h
+++ include/myBigChars.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#include <myTerm.h>
+
+const char encodingTable[18] = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                                 '9', 'A', 'B', 'C', 'D', 'E', 'F', '+', '-' };
+
 int bc_strlen (const char *str);
+int bc_box (int x1, int y1, int x2, int y2, enum colors box_fg,
+            enum colors box_bg, char *header, enum colors header_fg,
+            enum colors header_bg);
--- myBigChars/myBigChars.c
+++ myBigChars/myBigChars.c
@@ -1,6 +1,7 @@
 #include <myBigChars.h>
 
-#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int
 bc_strlen (const char *str)
@@ -58,4 +59,60 @@ bc_strlen (const char *str)
     }
 
   return count;
+}
+
+int
+bc_box (int x1, int y1, int x2, int y2, enum colors box_fg, enum colors box_bg,
+        char *header, enum colors header_fg, enum colors header_bg)
+{
+  char buffer[100];
+  int bufferLen;
+
+  mt_setfgcolor (box_fg);
+  mt_setbgcolor (box_bg);
+
+  write (STDOUT_FILENO, "\E(0", 3);
+
+  // draw top border
+  bufferLen = snprintf (buffer, sizeof (buffer), "\E[%d;%dHl", x1, y1);
+  write (STDOUT_FILENO, buffer, bufferLen);
+  for (int i = 1; i < y2 - 1; i++)
+    {
+      write (STDOUT_FILENO, "q", 1);
+    }
+  write (STDOUT_FILENO, "k", 1);
+
+  // draw side borders
+  for (int i = 1; i < x2 - 1; i++)
+    {
+      bufferLen = snprintf (buffer, sizeof (buffer), "\E[%d;%dHx", x1 + i, y1);
+      write (STDOUT_FILENO, buffer, bufferLen);
+      bufferLen = snprintf (buffer, sizeof (buffer), "\E[%d;%dHx", x1 + i,
+                            y1 + y2 - 1);
+      write (STDOUT_FILENO, buffer, bufferLen);
+    }
+
+  // draw bottom border
+  bufferLen
+      = snprintf (buffer, sizeof (buffer), "\E[%d;%dHm", x1 + x2 - 1, y1);
+  write (STDOUT_FILENO, buffer, bufferLen);
+  for (size_t i = 1; i < y2 - 1; i++)
+    {
+      write (STDOUT_FILENO, "q", 1);
+    }
+  write (STDOUT_FILENO, "j", 1);
+
+  write (STDOUT_FILENO, "\E(B", 3);
+
+  int headerLen;
+  if (header != NULL && (headerLen = bc_strlen (header)))
+    {
+      mt_setfgcolor (header_fg);
+      mt_setbgcolor (header_bg);
+      mt_gotoXY (x1, y1 + (y2 - headerLen) / 2);
+      write (STDOUT_FILENO, header, headerLen);
+    }
+
+  mt_setdefaultcolor ();
+  return 0;
 }
